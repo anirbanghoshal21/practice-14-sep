c58307af8620d47f208c9dfd3e2ad27237286574
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-15 20:51:19 +0530

    Problem 1

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomerUser;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.IO;
+
+namespace CustomerUser
+{
+    internal class Program
+    {
+        private static void binarySerialization()
+        {
+            Customer cust1 = new Customer();
+            cust1.UserId = 101;
+            cust1.Password = "myPassAmi";
+            bool validate = cust1.loginstatus();
+
+            FileStream fs = new FileStream(@"loginstatus.bin", FileMode.Create, FileAccess.Write);
+            BinaryFormatter formatter = new BinaryFormatter();
+            formatter.Serialize(fs, cust1);
+
+            fs.Flush();
+            fs.Close();
+            fs.Dispose();
+
+        }
+    }
+}
