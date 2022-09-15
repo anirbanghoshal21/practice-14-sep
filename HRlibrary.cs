b0a61185853e81d70ad7f78317051b0f342de0b2
anirbanghoshal21 <111297775+anirbanghoshal21@users.noreply.github.com>
2022-09-15 22:29:51 +0530

    prob 4

---
+++ HRlibrary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerUser
+{
+    [Serializable()]
+    public class Customer
+    {
+       public int UserId { get; set; }
+        public string Password { get; set; }
+        public   int Date { get; set; } 
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CustomerUser;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+using System.IO;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+
+namespace CustomerUser
+{
+    internal class Program
+    {
+        
+        
+        private static void SoapSerialize()
+        {
+            Stock sto1 = new Stock();
+            sto1.id = 102;
+            sto1.AvailableStock();
+
+            FileStream fs2 = new FileStream("empdataSoap.soap", FileMode.Create, FileAccess.Write);
+
+            SoapFormatter soap = new SoapFormatter();
+            soap.Serialize(fs2, sto1);
+            fs2.Flush();
+            fs2.Close();
+            fs2.Dispose();
+        }
+
+        private class Stock
+        {
+        }
+    }
+}
